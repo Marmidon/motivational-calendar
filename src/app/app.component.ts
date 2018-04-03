60d0d81aceef628ab945c2b4b312e4361599796e
Marmidon <nstenko1991@gmail.com>
2018-04-02 21:51:21 -0400

    dependancies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,43 +1,46 @@
-import { Component, ViewChild } from '@angular/core';
-import { BirthdayDateComponent } from './birthday-date/birthday-date.component';
-import * as moment from 'moment';
+import { Component, ViewChild, AfterViewInit, QueryList } from "@angular/core";
+import { BirthdayDateComponent } from "./birthday-date/birthday-date.component";
+import * as moment from "moment";
+import { ResultPanelComponent } from "./result-panel/result-panel.component";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  @ViewChild(BirthdayDateComponent) private birthdayComponent: BirthdayDateComponent;
+export class AppComponent implements AfterViewInit {
+  @ViewChild(BirthdayDateComponent)
+  private birthdayComponent: BirthdayDateComponent;
+  @ViewChild(ResultPanelComponent)
+  private resultComponent: QueryList<ResultPanelComponent>;
   birthday;
   targetAge = 70;
   target_date;
   weeks;
-  showResult = false;
   loading = false;
-  targetAges = [
-    60,
-    65,
-    70,
-    75,
-    80
-  ];
+  targetAges = [60, 65, 70, 75, 80];
   targetAgeToPassToResult;
   public generate = function() {
     this.loading = true;
     this.birthday = this.birthdayComponent.GetDateOfBirth();
     this.target_date = moment(this.birthday);
-    this.target_date.add(this.targetAge, 'years');
-    this.weeks = this.birthday.diff(this.target_date, 'weeks');
+    this.target_date.add(this.targetAge, "years");
+    this.weeks = this.birthday.diff(this.target_date, "weeks");
     let now = moment();
-    this.weeksRemaining = this.target_date.diff(now, 'weeks');
+    this.weeksRemaining = this.target_date.diff(now, "weeks");
     if (this.weeksRemaining <= 0) {
       this.targetAge = this.targetAges[this.targetAges.length - 1];
       this.generate();
       return;
     }
-    this.showResult = true;
     this.targetAgeToPassToResult = this.targetAge;
-    this.loading = false;
+    this.resultComponent.generateResult(this.birthday, this.targetAge);
   };
+
+  ngAfterViewInit() {
+
+  }
+  ngForRendred() {
+
+  }
 }
--- src/app/result-panel/result-panel.component.ts
+++ src/app/result-panel/result-panel.component.ts
@@ -1,45 +1,46 @@
-import { Component, OnInit, Input, OnChanges, NgModule, Output, EventEmitter } from '@angular/core';
-import * as moment from 'moment';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import * as $ from 'jquery';
+import {
+  Component,
+  OnInit,
+  Input,
+  OnChanges,
+  NgModule,
+  Output,
+  EventEmitter,
+  AfterViewChecked,
+  AfterViewInit
+} from "@angular/core";
+import * as moment from "moment";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import * as $ from "jquery";
 
 @Component({
-  selector: 'app-result-panel',
-  templateUrl: './result-panel.component.html',
-  styleUrls: ['./result-panel.component.css']
+  selector: "app-result-panel",
+  templateUrl: "./result-panel.component.html",
+  styleUrls: ["./result-panel.component.css"]
 })
-
 @NgModule({
-  imports: [
-    NgbModule.forRoot()
-  ]
+  imports: [NgbModule.forRoot()]
 })
-
-
-
-
-export class ResultPanelComponent implements OnChanges {
-  @Input() birthDate: moment.Moment;
-  @Input() targetAge: number;
-  @Output()
-    loadingChange: EventEmitter<boolean> = new EventEmitter<boolean>();
-  @Input()
-    loading: boolean;
+export class ResultPanelComponent
+  implements OnChanges, AfterViewChecked, AfterViewInit {
+  birthDate: moment.Moment;
+  targetAge: number;
+  @Output() loadingChange: EventEmitter<boolean> = new EventEmitter<boolean>();
+  @Input() loading: boolean;
   target_date: moment.Moment;
   weeksPassedBetweenBirthAndNow;
   weeksBetweenBirthAndTarget;
   weeksRemaining;
   now1;
-  weeksPassedArray = [];
-  weeksRemainingArray = [];
+  show = false;
   constructor() {
     this.target_date = moment();
-   }
+  }
   print() {
     let printContents, popupWin, printTitle;
-    printTitle = document.getElementById('print-title').innerHTML;
-    printContents = document.getElementById('printable').innerHTML;
-    popupWin = window.open('', '_blank', 'top=0,left=0,height=100%,width=auto');
+    printTitle = document.getElementById("print-title").innerHTML;
+    printContents = document.getElementById("printable").innerHTML;
+    popupWin = window.open("", "_blank", "top=0,left=0,height=100%,width=auto");
     popupWin.document.open();
     popupWin.document.write(`
       <html>
@@ -58,30 +59,48 @@ export class ResultPanelComponent implements OnChanges {
         </div>
           ${printContents}
       </body>
-      </html>`
-    );
+      </html>`);
     popupWin.document.close();
   }
 
-  ngOnChanges() {
+  ngOnChanges() {}
+
+  public generateResult(_birthDate: moment.Moment, _targetAge: number) {
+    this.show = false;
+    this.birthDate = _birthDate;
+    this.targetAge = _targetAge;
+
     if (this.targetAge && this.birthDate) {
       this.target_date = moment(this.birthDate);
-      this.target_date.add(this.targetAge, 'years');
-      this.weeksBetweenBirthAndTarget = this.target_date.diff(this.birthDate, 'weeks');
+      this.target_date.add(this.targetAge, "years");
+      this.weeksBetweenBirthAndTarget = this.target_date.diff(
+        this.birthDate,
+        "weeks"
+      );
       let now = moment();
       this.now1 = now;
-      this.weeksPassedBetweenBirthAndNow = now.diff(this.birthDate, 'weeks');
-      this.weeksRemaining = this.target_date.diff(now, 'weeks');
-      this.weeksPassedArray = [];
-      for (let i = 1; i <= this.weeksPassedBetweenBirthAndNow; i++) {
-        this.weeksPassedArray.push(i);
-      }
-      this.weeksRemainingArray = [];
-      for (let i = 1; i <= this.weeksRemaining; i++) {
-        this.weeksRemainingArray.push(i);
-      }
-      this.loadingChange.emit(this.loading);
+      this.weeksPassedBetweenBirthAndNow = now.diff(this.birthDate, "weeks");
+      this.weeksRemaining = this.target_date.diff(now, "weeks");
     }
+    $("#printable").empty();
+    for (let i = 1; i <= this.weeksPassedBetweenBirthAndNow; i++) {
+      $("#printable").append('<img src="assets\\img\\Checked.png" alt="">');
+    }
+    for (let i = 1; i <= this.weeksRemaining; i++) {
+      $("#printable").append('<img src="assets\\img\\Unchecked.png" alt="">');
+    }
+    if (document.getElementById("print-title")) {
+      document
+        .getElementById("print-title")
+        .scrollIntoView({ behavior: "smooth" });
+    }
+    this.loadingChange.emit(false);
+
+    this.show = true;
   }
 
+  ngAfterViewChecked() {}
+  ngAfterViewInit() {
+
+  }
 }
